2111bb51f6ea176828d352b5747665dd5cf869e3
Cristian Camilo <cristian_cortes_ortiz@hotmail.com>
2023-10-31 06:38:36 -0500

    objeto anjeo creado y funciones de descuento

---
+++ code/anjeos-obj/user-inputs.js
@@ -0,0 +1,25 @@
+// const inputWidth = document.getElementById('').value;
+const ticketIdInput = 15631;
+const placeInput = "Cocina";
+const widthInput = 60;
+const heightInput = 120;
+const headInput = 1;
+const guidesInput = 1;
+const adaptadorInput = 1;
+const topProfileInput =1;
+
+const divisorInput = 60;
+const noteInput = "Algún comentario del cliente..";
+
+module.exports = {
+    ticketIdInput,
+    placeInput,
+    widthInput,
+    heightInput,
+    headInput,
+    guidesInput,
+    adaptadorInput,
+    topProfileInput,
+    divisorInput,
+    noteInput,
+};
